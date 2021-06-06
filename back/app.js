9d98ca479d0c860c052a89ae50cfa9027788bc8e
dlrbcnvk <dlrbcnvk@naver.com>
2021-06-06 16:04:56 +0900

    app.js package.json

--- back/app.js
+++ back/app.js
@@ -47,11 +47,6 @@ app.use(session({
   saveUninitialized: false,
   resave: false,
   secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-    domain: process.env.NODE_ENV === 'production' && '.nodering.com'
-  },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
