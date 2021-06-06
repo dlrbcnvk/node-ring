99569e92daf007ea272eaeff40e6f018b0428d49
dlrbcnvk <dlrbcnvk@naver.com>
2021-06-06 16:02:03 +0900

    app.js package.json

--- back/app.js
+++ back/app.js
@@ -29,13 +29,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(hpp());
   app.use(helmet({ contentSecurityPolicy: false }));
   app.use(cors({
-    origin: ['http://3.34.44.195', 'http://nodering.com' ],
+    origin: ['http://54.180.107.99', 'http://nodering.com' ],
     credentials: true,
   }));
 } else {
   app.use(morgan('dev'));
   app.use(cors({
-    origin: true,
+    origin: 'http://localhost:3000',
     credentials: true,
   }));
 }
