dd9d6325ed4f062a268a26e540e7d261d1287fcf
dlrbcnvk <dlrbcnvk@naver.com>
2021-06-11 18:09:55 +0900

    please

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,28 +1,24 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddlesare from 'redux-saga';
+import { applyMiddleware, createStore, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import { createWrapper } from 'next-redux-wrapper';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from '../reducers';
 import rootSaga from '../sagas';
 
-const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
-  console.log(action);
-  return next(action);
-};
-
 const configureStore = () => {
-  const sagaMiddleware = createSagaMiddlesare();
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer = process.env.NODE_ENV === 'production'
-    ? compose(applyMiddleware(sagaMiddleware))
-    : composeWithDevTools(applyMiddleware(sagaMiddleware, loggerMiddleware));
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(
+      applyMiddleware(...middlewares),
+    );
   const store = createStore(reducer, enhancer);
   store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
-const wrapper = createWrapper(configureStore, {
-  debug: process.env.NODE_ENV === 'development',
-});
+const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development' });
 
 export default wrapper;
